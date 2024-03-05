dbc1e7d93a1a4423ec47d2fe17cc8ba8025c13e5
turuhsen <tubaaldemir1013@gmail.com>
2024-03-05 09:43:50 +0100

    linktest added,ExplicitWait added

--- src/Utilty/Myfunc.java
+++ src/Utilty/Myfunc.java
@@ -5,7 +5,7 @@ public class Myfunc {
     public static void Bekle(int sn){
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(1000*sn);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
